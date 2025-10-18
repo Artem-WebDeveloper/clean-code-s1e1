9969aa70ea66f95ee9155ad8ebc5ea3e0d7aaf52
Artem-WebDeveloper <ave_art@icloud.com>
2025-10-19 01:08:57 +0300

    refactor(extended-1.1): replace label tags without input association with span for semantics

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ var createNewTaskElement=function(taskString){
     //input (checkbox)
     var checkBox=document.createElement("input");//checkbx
     //label
-    var label=document.createElement("label");//label
+    var label=document.createElement("span");//label
     //input (text)
     var editInput=document.createElement("input");//text
     //button.edit
@@ -88,7 +88,7 @@ var editTask=function(){
     var listItem=this.parentNode;
 
     var editInput=listItem.querySelector('input[type=text]');
-    var label=listItem.querySelector("label");
+    var label=listItem.querySelector(".task-list__label");
     var editBtn=listItem.querySelector(".task-button--edit");
     var containsClass=listItem.classList.contains("task-list__item--edit");
     //If class of the parent is .editmode
